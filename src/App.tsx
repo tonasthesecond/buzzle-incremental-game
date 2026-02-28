43354eefd998f7707cc8c7cbba7966a2908e8676
codebender <133840949+codebender04@users.noreply.github.com>
2026-02-28 14:21:04 +0700

    add a bee button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { GameCanvas } from "./ui/GameCanvas";
+import { addBee } from "./systems/beeSystem";
 
 export default function App() {
   return (
     <div style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
-      <GameCanvas />
+      <GameCanvas/>
       <div style={{ color: "white" }}>UI goes here</div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <button
+          onClick={addBee}
+          style={{
+            backgroundColor: "#f5c542",
+            borderRadius: "8px",
+            border: "none",
+          }}
+        >
+          Add Bee
+        </button>
+      </div>
     </div>
   );
 }
