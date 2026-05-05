8025a158b547695301ee5d4f3e483d9e569be330
Thai-Anh-Nguyen <thaianhwastaken@gmail.com>
2026-05-06 00:23:11 +0700

    update Firebase configuration with actual API key and project ID

--- scripts/Firebase/FirebaseConfig.cs
+++ scripts/Firebase/FirebaseConfig.cs
@@ -1,7 +1,7 @@
 public static class FirebaseConfig
 {
-    public const string ApiKey = "YOUR_FIREBASE_API_KEY";
-    public const string ProjectId = "YOUR_PROJECT_ID";
+    public const string ApiKey = "AIzaSyCXGvOOeHWvxmALZQgJgCaN5keo3UZGg14";
+    public const string ProjectId = "buzzle-b8128";
     public const string AuthBase = "https://identitytoolkit.googleapis.com/v1/accounts";
     public const string TokenBase = "https://securetoken.googleapis.com/v1/token";
     public const string StoreBase =
